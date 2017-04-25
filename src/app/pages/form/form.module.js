8a829dab63aa828581d3505c529cbc1c914e75eb
Darcelsan <darc@live.fr>
2017-04-25 17:02:40 +0200

    Page inscription et quelques détails

--- src/app/pages/form/form.module.js
+++ src/app/pages/form/form.module.js
@@ -15,7 +15,7 @@
           url: '/form',
           template : '<ui-view autoscroll="true" autoscroll-body-top></ui-view>',
           abstract: true,
-          title: 'Form Elements',
+          title: 'Inscription',
           sidebarMeta: {
             icon: 'ion-compose',
             order: 250,
@@ -24,29 +24,30 @@
         .state('form.inputs', {
           url: '/inputs',
           templateUrl: 'app/pages/form/inputs/inputs.html',
-          title: 'Form Inputs',
+          title: 'Inscription Newsletter',
           sidebarMeta: {
             order: 0,
           },
         })
-        .state('form.layouts', {
-          url: '/layouts',
-          templateUrl: 'app/pages/form/layouts/layouts.html',
-          title: 'Form Layouts',
-          sidebarMeta: {
-            order: 100,
-          },
-        })
-        .state('form.wizard',
-        {
-          url: '/wizard',
-          templateUrl: 'app/pages/form/wizard/wizard.html',
-          controller: 'WizardCtrl',
-          controllerAs: 'vm',
-          title: 'Form Wizard',
-          sidebarMeta: {
-            order: 200,
-          },
-        });
+        // .state('form.layouts', {
+        //   url: '/layouts',
+        //   templateUrl: 'app/pages/form/layouts/layouts.html',
+        //   title: 'Form Layouts',
+        //   sidebarMeta: {
+        //     order: 100,
+        //   },
+        // })
+        // .state('form.wizard',
+        // {
+        //   url: '/wizard',
+        //   templateUrl: 'app/pages/form/wizard/wizard.html',
+        //   controller: 'WizardCtrl',
+        //   controllerAs: 'vm',
+        //   title: 'Form Wizard',
+        //   sidebarMeta: {
+        //     order: 200,
+        //   },
+        // })
+        ;
   }
 })();

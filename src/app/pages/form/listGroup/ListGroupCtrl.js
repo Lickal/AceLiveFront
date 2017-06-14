d22a938265061a0c3225c0091859f9258d5dfaca
Lickall <lickal.34@gmail.com>
2017-06-14 21:09:22 +0200

    MAJ listCampagne listGroup listNewsletter

--- src/app/pages/form/listGroup/ListGroupCtrl.js
+++ src/app/pages/form/listGroup/ListGroupCtrl.js
@@ -7,9 +7,32 @@
     /** @ngInject */
     function ListGroupCtrl($scope, $http, $timeout, $element) {
 
-        $scope.addMail = function () {
-            // TODO
-            console.log("Test");
-        };
+        $scope.smartTablePageSize = 10;
+
+        $scope.smartTableData = [
+            {
+                id: 1,
+                firstName: 'Mark',
+                lastName: 'Otto',
+                username: '@mdo',
+                age: '28'
+            },
+            {
+                id: 2,
+                firstName: 'Jacob',
+                lastName: 'Thornton',
+                username: '@fat',
+                age: '45'
+            },
+            {
+                id: 3,
+                firstName: 'Larry',
+                lastName: 'Bird',
+                username: '@twitter',
+                age: '18'
+            }
+        ];
+
+        $scope.editableTableData = $scope.smartTableData.slice(0, 36);
     }
 })();
